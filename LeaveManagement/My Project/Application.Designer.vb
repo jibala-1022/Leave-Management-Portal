b8fce15e5f2d0e3f7cf8ec9393b14dd5901cd053
sathwik <sathwik.19120@gmail.com>
2024-03-03 21:20:39 +0530

    nothing

--- LeaveManagement/My Project/Application.Designer.vb
+++ LeaveManagement/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.LeaveManagement.student_registration
+            Me.MainForm = Global.LeaveManagement.Login
         End Sub
     End Class
 End Namespace
